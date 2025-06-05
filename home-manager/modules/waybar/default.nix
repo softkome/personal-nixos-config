e84f5fffa0397b8e931b57364fb0df577a2be4b9
softkome <softkome+git@gmail.com>
2025-06-05 04:28:39 +0000

    Create default.nix

---
+++ home-manager/modules/waybar/default.nix
@@ -0,0 +1,88 @@
+{
+  programs.waybar = {
+    enable = true;
+    style = ./style.css;
+    settings = {
+      mainBar = {
+        layer = "top";
+        position = "top";
+        height = 30;
+        modules-left = ["hyprland/workspaces"];
+        modules-center = ["hyprland/window"];
+        modules-right = ["custom/microphone" "pulseaudio" "network" "custom/power" "clock" "tray"];
+        "hyprland/workspaces" = {
+          disable-scroll = true;
+          show-special = true;
+          special-visible-only = true;
+          all-outputs = false;
+          format = "{icon}";
+          format-icons = {
+            "1" = "1"; 
+            "2" = "2";
+            "3" = "3";
+            "4" = "4";
+            "5" = "5";
+            "magic" = "";
+          };
+
+          persistent-workspaces = {
+            "*" = 5;
+          };
+        };
+
+	"custom/power" = {
+	  format = "<span size='large'>⏻</span>";
+	  on-click = "~/.config/waybar/scripts/power-menu.sh";
+          tooltip = false;
+	};
+
+	"network" = {
+	   format-wifi = "󰤨 {essid}";
+	   format-ethernet = "󰈀 Wired";
+ 	   format-disconnected = "󰤮 Disconnected";
+	   format-alt = "󰇚 {bandwidthDownBits} 󰕒 {bandwidthUpBits}";
+  	   tooltip-format = "Interface: {ifname}\nIP: {ipaddr}\nSignal: {signalStrength}%";
+	   tooltip-format-ethernet = "Interface: {ifname}\nIP: {ipaddr}";
+	   tooltip-format-disconnected = "No connection";
+	   interval = 2;
+	};
+
+        "custom/microphone" = {
+          exec = "~/.config/waybar/scripts/microphone.sh";
+          return-type = "json";
+          interval = 1;
+          format = "{}";
+          on-click = "pactl set-source-mute @DEFAULT_SOURCE@ toggle";
+          tooltip = true;
+        };
+
+        "pulseaudio" = {
+          format = "{icon} {volume}%";
+          format-bluetooth = "{icon} {volume}% ";
+          format-muted = "";
+          format-icons = {
+            "headphones" = "";
+            "handsfree" = "";
+            "headset" = "";
+            "phone" = "";
+            "portable" = "";
+            "car" = "";
+            "default" = ["" ""];
+          };
+          on-click = "pavucontrol";
+        };
+
+        "clock" = {
+          format = "{:%d.%m.%Y | %H:%M}";
+          format-alt = "{:%A, %B %d at %R}";
+	  tooltip = false;
+        };
+
+        "tray" = {
+          icon-size = 14;
+          spacing = 2;
+        };
+      };
+    };
+  };
+}
