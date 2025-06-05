107801807be3a54b42432a69f60d6b56fdb67d46
softkome <softkome+git@gmail.com>
2025-06-05 01:36:25 +0000

    Create env.nix

---
+++ home-manager/modules/env.nix
@@ -0,0 +1,10 @@
+{ config, ... }:
+
+{
+  home.sessionVariables = {
+    TERMINAL = "alacritty";
+    EDITOR = "nvim";
+    XDG_BIN_HOME = "${config.home.homeDirectory}/.local/bin";
+    PATH = [ "${config.home.homeDirectory}/.local/bin" ];
+  };
+}
