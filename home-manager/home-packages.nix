776d2ca9e2afcae8f26c8a2f7897974260688dff
softkome <softkome+git@gmail.com>
2025-06-05 04:42:40 +0000

    Update home-packages.nix

--- home-manager/home-packages.nix
+++ home-manager/home-packages.nix
@@ -1,1 +1,67 @@
+{ pkgs, ... }: {
+  nixpkgs.config.allowUnfree = true;
 
+  home.packages = with pkgs; [
+
+    #desktop apps
+    librewolf
+    imv
+    mpv
+    obs-studio
+    pavucontrol
+    vesktop
+    stremio
+
+    #cli utils
+    bc
+    bottom
+    brightnessctl
+    cliphist
+    ffmpeg
+    ffmpegthumbnailer
+    fzf
+    git-graph
+    grimblast
+    htop
+    hyprpicker
+    ntfs3g
+    mediainfo
+    microfetch
+    playerctl
+    ripgrep
+    showmethekey
+    silicon
+    udisks
+    ueberzugpp
+    unzip
+    unrar
+    w3m
+    wget
+    wl-clipboard
+    wtype
+    yt-dlp
+    zip
+    cava
+    cmatrix
+    tt
+
+    #coding stuff
+    openjdk23
+    nodejs
+    python311
+    nixd
+
+    #wm stuff
+    libsForQt5.xwaylandvideobridge
+    libnotify
+    xdg-desktop-portal-gtk
+    xdg-desktop-portal-hyprland
+
+    #other
+    bemoji
+    nix-prefetch-scripts
+    protonup
+    mangohud
+    scientifica
+  ];
+}
