eb41da7eadf8d6c7000c3d4ff92b461ff73a4e3d
softkome <softkome+git@gmail.com>
2025-06-05 01:04:21 +0000

    Create flake.nix

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "nixos clean config";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    stylix = {
+      url = "github:danth/stylix/release-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, stylix, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      hosts = {
+        "nixos-desktop" = {
+          user = "softkome";
+          hostname = "nixos-desktop";
+          stateVersion = "24.11";
+        };
+      };
+
+      mkHost = name: cfg:
+        nixpkgs.lib.nixosSystem {
+          system = system;
+          specialArgs = {
+            inherit inputs system;
+            user = cfg.user;
+            hostname = cfg.hostname;
+            homeStateVersion = cfg.stateVersion;
+          };
+
+          modules = [
+            ./hosts/${name}/configuration.nix
+          ];
+        };
+
+    in {
+      nixosConfigurations = nixpkgs.lib.mapAttrs mkHost hosts;
+    };
+}
