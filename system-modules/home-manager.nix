85fe2a9d2c751ae83bf88e242de3acd8500855d7
softkome <softkome+git@gmail.com>
2025-06-05 02:52:56 +0000

    Create home-manager.nix

---
+++ system-modules/home-manager.nix
@@ -0,0 +1,9 @@
+{ inputs, user, homeStateVersion, ... }: {
+  home-manager = {
+    extraSpecialArgs = {
+      inherit inputs user homeStateVersion;
+    };
+
+    users.${user} = import ../../home-manager/home.nix;
+  };
+}
