bc499235ebd41183f696a9f76ca1510b4c8e3f70
softkome <softkome+git@gmail.com>
2025-06-05 00:46:58 +0000

    Update configuration.nix

--- hosts/softkome/configuration.nix
+++ hosts/softkome/configuration.nix
@@ -1,1 +1,32 @@
+{ config, pkgs, inputs, user, hostname, homeStateVersion, ... }:
 
+{
+  imports = [
+    ./hardware-configuration.nix
+    ../../nixos-modules
+    inputs.home-manager.nixosModules.default
+  ];
+
+  networking.hostName = hostname;
+
+  time.timeZone = "Europe/London";
+
+  i18n = {
+    defaultLocale = "en_GB.UTF-8";
+    extraLocaleSettings = {
+      LC_ALL = "en_GB.UTF-8";
+    };
+  };
+
+  nixpkgs.config.allowUnfree = true;
+
+  home-manager = {
+    extraSpecialArgs = {
+      inherit inputs user homeStateVersion;
+    };
+
+    users.${user} = import ../../home-manager/home.nix;
+  };
+
+  system.stateVersion = "24.11";
+}
