c1a932aa60217faa23503a430a1632b09a89d16f
softkome <softkome+git@gmail.com>
2025-06-05 02:56:17 +0000

    Update configuration.nix

--- hosts/nixos-desktop/configuration.nix
+++ hosts/nixos-desktop/configuration.nix
@@ -10,24 +10,7 @@
 
   networking.hostName = hostname;
 
-  time.timeZone = "Europe/London";
-
-  i18n = {
-    defaultLocale = "en_GB.UTF-8";
-    extraLocaleSettings = {
-      LC_ALL = "en_GB.UTF-8";
-    };
-  };
-
-  nixpkgs.config.allowUnfree = true;
-
-  home-manager = {
-    extraSpecialArgs = {
-      inherit inputs user homeStateVersion;
-    };
-
-    users.${user} = import ../../home-manager/home.nix;
-  };
-
   system.stateVersion = "24.11";
+  user = "softkome";
+  inputs = inputs;
 }
