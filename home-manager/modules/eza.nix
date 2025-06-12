d45014e2285af227ff418e9bb9062291fc50dcc9
softkome <softkome+git@gmail.com>
2025-06-12 22:28:20 +0000

    Update eza.nix

--- home-manager/modules/eza.nix
+++ home-manager/modules/eza.nix
@@ -1,13 +1,20 @@
-{
-  programs.eza = {
-    enable = true;
-    enableFishIntegration = true;
-    colors = "always";
-    git = true;
-    icons = "always";
-    extraOptions = [
-      "--group-directories-first"
-      "--header"
-    ];
+{ lib, config, ...}: {
+
+  options = {
+    ezaMod = lib.mkEnableOption "enables ezaMod";
+  };
+
+  config = lib.mkIf config.ezaMod {
+    programs.eza = {
+      enable = true;
+      enableFishIntegration = true;
+      colors = "always";
+      git = true;
+      icons = "always";
+      extraOptions = [
+        "--group-directories-first"
+        "--header"
+      ];
+    };
   };
 }
