9ccb1275d28871939e4da539fa382d51a01d6427
softkome <softkome+git@gmail.com>
2025-06-12 22:27:58 +0000

    Update env.nix

--- home-manager/modules/env.nix
+++ home-manager/modules/env.nix
@@ -1,10 +1,15 @@
-{ config, ... }:
+{ lib, config, ... }: {
 
-{
-  home.sessionVariables = {
-    TERMINAL = "alacritty";
-    EDITOR = "nvim";
-    XDG_BIN_HOME = "${config.home.homeDirectory}/.local/bin";
-    PATH = "${config.home.homeDirectory}/.local/bin:$PATH";
+  options = {
+    envMod = lib.mkEnableOption "enables envMod";
+  };
+
+  config = lib.mkIf config.envMod {
+    home.sessionVariables = {
+      TERMINAL = "alacritty";
+      EDITOR = "nvim";
+      XDG_BIN_HOME = "${config.home.homeDirectory}/.local/bin";
+      PATH = "${config.home.homeDirectory}/.local/bin:$PATH";
+    };
   };
 }
