95ac374736b78affd6706d60ccb13f62abcb0cac
softkome <softkome+git@gmail.com>
2025-06-12 22:19:58 +0000

    Update hypridle.nix

--- home-manager/modules/hyprland/hypridle.nix
+++ home-manager/modules/hyprland/hypridle.nix
@@ -1,34 +1,41 @@
-{
-  services.hypridle = {
-    enable = true;
-    settings = {
-      general = {
-        before_sleep_cmd = "loginctl lock-session";
-        after_sleep_cmd = "hyprctl dispatch dpms on";
-        ignore_dbus_inhibit = false;
-        lock_cmd = "pidof hyprlock || hyprlock";
-      };
+{ lib, config, ... }: {
 
-      listener = [
-        {
-          timeout = 180;
-          "on-timeout" = "brightnessctl -s set 30";
-          "on-resume" = "brightnessctl -r";
-        },
-        {
-          timeout = 300;
-          "on-timeout" = "loginctl lock-session";
-        },
-        {
-          timeout = 600;
-          "on-timeout" = "hyprctl dispatch dpms off";
-          "on-resume" = "hyprctl dispatch dpms on";
-        },
-        {
-          timeout = 1200;
-          "on-timeout" = "systemctl suspend";
-        },
-      ];
+  options = {
+    hypridleMod = lib.mkEnableOption "enables hypridleMod";
+  };
+
+  config = lib.mkIf config.hypridleMod {
+    services.hypridle = {
+      enable = true;
+      settings = {
+        general = {
+          before_sleep_cmd = "loginctl lock-session";
+          after_sleep_cmd = "hyprctl dispatch dpms on";
+          ignore_dbus_inhibit = false;
+          lock_cmd = "pidof hyprlock || hyprlock";
+        };
+  
+        listener = [
+          {
+            timeout = 180;
+            "on-timeout" = "brightnessctl -s set 30";
+            "on-resume" = "brightnessctl -r";
+          }
+          {
+            timeout = 300;
+            "on-timeout" = "loginctl lock-session";
+          }
+          {
+            timeout = 600;
+            "on-timeout" = "hyprctl dispatch dpms off";
+            "on-resume" = "hyprctl dispatch dpms on";
+          }
+          {
+            timeout = 1200;
+            "on-timeout" = "systemctl suspend";
+          }
+        ];
+      };
     };
   };
 }
