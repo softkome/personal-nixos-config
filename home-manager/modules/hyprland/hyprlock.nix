2f500fd4176d81b44563f989fc47fac3999d7bfa
softkome <softkome+git@gmail.com>
2025-06-12 22:19:30 +0000

    Update hyprlock.nix

--- home-manager/modules/hyprland/hyprlock.nix
+++ home-manager/modules/hyprland/hyprlock.nix
@@ -1,48 +1,55 @@
-{
-  programs.hyprlock = {
-    enable = true;
+{ lib, config, ... }: {
 
-    settings = {
-      general = {
-        disable_loading_bar = true;
-        grace = 10;
-        hide_cursor = true;
-        no_fade_in = false;
-      };
+  options = {
+    hyprlockMod = lib.mkEnableOption "enables hyprlockMod";
+  };
 
-      label = {
-        text = "$TIME";
-        font_size = 96;
-        font_family = "JetBrains Mono";
-        color = "rgba(235, 219, 178, 1.0)";
-        position = "0, 600";
-        halign = "center";
-        walign = "center";
-        shadow_passes = 1;
-      };
-
-      background = [
-        {
-          path = "screenshot";
-          blur_passes = 3;
-          blur_size = 8;
-        }
-      ];
-
-      input-field = [
-        {
-          size = "200, 50";
-          position = "0, -80";
-          monitor = "";
-          dots_center = true;
-          font_color = "rgb(235, 219, 178)";
-          inner_color = "rgb(40, 40, 40)";
-          outer_color = "rgb(60, 56, 54)";
-          outline_thickness = 5;
-          placeholder_text = "sussy baka";
+  config = lib.mkIf config.hyprlockMod {
+    programs.hyprlock = {
+      enable = true;
+  
+      settings = {
+        general = {
+          disable_loading_bar = true;
+          grace = 10;
+          hide_cursor = true;
+          no_fade_in = false;
+        };
+  
+        label = {
+          text = "$TIME";
+          font_size = 96;
+          font_family = "JetBrains Mono";
+          color = "rgba(235, 219, 178, 1.0)";
+          position = "0, 600";
+          halign = "center";
+          walign = "center";
           shadow_passes = 1;
-        }
-      ];
+        };
+  
+        background = [
+          {
+            path = "screenshot";
+            blur_passes = 3;
+            blur_size = 8;
+          }
+        ];
+  
+        input-field = [
+          {
+            size = "200, 50";
+            position = "0, -80";
+            monitor = "";
+            dots_center = true;
+            font_color = "rgb(235, 219, 178)";
+            inner_color = "rgb(40, 40, 40)";
+            outer_color = "rgb(60, 56, 54)";
+            outline_thickness = 5;
+            placeholder_text = "password";
+            shadow_passes = 1;
+          }
+        ];
+      };
     };
   };
 }
