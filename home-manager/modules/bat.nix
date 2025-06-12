44ba9d2b135a1b05b345e3de7e529e2b77965802
softkome <softkome+git@gmail.com>
2025-06-12 22:25:25 +0000

    Update bat.nix

--- home-manager/modules/bat.nix
+++ home-manager/modules/bat.nix
@@ -1,15 +1,22 @@
-{ pkgs, ... }: {
-  programs.bat = {
-    enable = true;
-    themes = {
-      dracula = {
-        src = pkgs.fetchFromGitHub {
-          owner = "Briles";
-          repo = "gruvbox";
-          rev = "75407cc80c51814d61beb1df07e380d6f58ad767";
-          sha256 = "186rhbljw80psf1l8hyj02ycz1wzxv4rxmbrqr8yvi30165drpay";
+{ pkgs, lib, config, ... }: {
+
+  options = {
+    batMod = lib.mkEnableOption "enables batMod";
+  };
+
+  config = lib.mkIf config.batMod {
+    programs.bat = {
+      enable = true;
+      themes = {
+        dracula = {
+          src = pkgs.fetchFromGitHub {
+            owner = "Briles";
+            repo = "gruvbox";
+            rev = "75407cc80c51814d61beb1df07e380d6f58ad767";
+            sha256 = "186rhbljw80psf1l8hyj02ycz1wzxv4rxmbrqr8yvi30165drpay";
+          };
+          file = "gruvbox (Dark) (Medium).sublime-color-scheme";
         };
-        file = "gruvbox (Dark) (Medium).sublime-color-scheme";
       };
     };
   };
