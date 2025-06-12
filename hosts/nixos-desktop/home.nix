303657ecd2fe24484e83a832106a56241e31a0d2
softkome <softkome+git@gmail.com>
2025-06-12 22:48:41 +0000

    Create home.nix

---
+++ hosts/nixos-desktop/home.nix
@@ -0,0 +1,41 @@
+{ user, homeStateVersion, ... }:
+
+{
+  imports = [
+    ../../home-manager/home-packages.nix
+    ../../home-manager/modules
+  ];
+
+  home = {
+    username = user;
+    homeDirectory = "/home/${user}";
+    stateVersion = homeStateVersion;
+  };
+
+  home.sessionVariables = {
+    TEAM_EXTRA_COMPAT_TOOLS_PATHS =
+      "\\\${HOME}/.steam/root/compatibilitytools.d";
+  };
+
+  tmuxMod.enable = true;
+  zathuraMod.enable = true;
+  rangerMod.enable = true;
+  fishMod = true;
+  gitMod.enable = true;
+  lazygitMod.enable = true;
+  neovimMod = true;
+  starshipMod.enable = true;
+  envMod = true;
+  ezaMod = true;
+  chromiumMod = true;
+  alacrittyMod = true;
+  batMod = true;
+
+  waybarMod = true;
+  swayncMod = true;
+  fuzzelMod = true;
+  hyprlandMod = true;
+  hyprlockMod = true;
+  hypridleMod = true;
+  hyprpaperMod = true;
+}
