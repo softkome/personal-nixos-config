2f0af7f03089b9393d5903276e8cc65d3b73d52a
softkome <softkome+git@gmail.com>
2025-06-12 22:47:36 +0000

    Update zathura.nix

--- home-manager/modules/zathura.nix
+++ home-manager/modules/zathura.nix
@@ -1,13 +1,20 @@
-{
-  programs.zathura = {
-    enable = true;
-    mappings = {
-      D = "toggle_page_mode";
-      d = "scroll half_down";
-      u = "scroll half_up";
-    };
-    options = {
-      font = "JetBrains Mono Bold 13";
+{ lib, config, ...}: {
+
+  options = {
+    zathuraMod.enable = lib.mkEnableOption "enables zathuraMod";
+  };
+
+  config = lib.mkIf config.zathuraMod.enable {
+    programs.zathura = {
+      enable = true;
+      mappings = {
+        D = "toggle_page_mode";
+        d = "scroll half_down";
+        u = "scroll half_up";
+      };
+      options = {
+        font = "JetBrains Mono Bold 13";
+      };
     };
   };
 }
