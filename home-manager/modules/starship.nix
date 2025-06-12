f6c0cf38a32e24e3485608ef36225188b14e076b
softkome <softkome+git@gmail.com>
2025-06-12 22:45:43 +0000

    Update starship.nix

--- home-manager/modules/starship.nix
+++ home-manager/modules/starship.nix
@@ -1,26 +1,49 @@
-{
-  programs.starship = {
-    enable = true;
-    enableZshIntegration = true;
-    settings = {
-      add_newline = true;
-      hostname = {
-        ssh_only = false;
-        format = "[$ssh_symbol$hostname]($style) ";
-        style = "bold purple";
-      };
-      character = {
-        success_symbol = "[ & ](bold green)";
-        error_symbol = "[ & ](bold red)";
-      };
-      username = {
-        show_always = true;
-        format = "[$user]($style)@";
-      };
-      directory = {
-        read_only = " 🔒";
-        truncation_symbol = "…/";
+{ lib, config, ...}: {
+
+  options = {
+    starshipMod.enable = lib.mkEnableOption "enables starshipMod";
+  };
+
+  config = lib.mkIf config.starshipMod.enable {
+    programs.starship = {
+      enable = true;
+      enableZshIntegration = true;
+      settings = {
+        add_newline = true;
+  
+        format = "$username$hostname$directory$git_branch$character";
+  
+        username = {
+          show_always = true;
+          format = "[▉](bg:background fg:none)[(bg:background fg:none) $user ](inverted)";
+          style_user = "bg:background fg:none";
+        };
+  
+        hostname = {
+          ssh_only = false;
+          format = "[▉](bg:background fg:none)[(bg:background fg:none) $hostname ](inverted)";
+          style = "bg:background fg:none";
+        };
+  
+        directory = {
+          format = "[▉](bg:background fg:none)[(bg:background fg:none) $path ](inverted)$read_only";
+          style = "bg:background fg:none";
+          read_only = " 🔒";
+          truncation_symbol = "…/";
+        };
+  
+        git_branch = {
+          format = "[▉](bg:background fg:none)[(bg:background fg:none) \ue725 $branch ](inverted)";
+          style = "bg:background fg:none";
+        };
+  
+        character = {
+          success_symbol = "[ & ](green bold)";
+          error_symbol = "[ & ](red)";
+          vicmd_symbol = "[ & ](yellow)";
+        };
       };
     };
   };
 }
+
