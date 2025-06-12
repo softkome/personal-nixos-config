ce21be23e0407d5032101dc641230ccbe38dc88d
softkome <softkome+git@gmail.com>
2025-06-12 22:25:02 +0000

    Update alacritty.nix

--- home-manager/modules/alacritty.nix
+++ home-manager/modules/alacritty.nix
@@ -1,16 +1,23 @@
-{lib, ... }:{
-  programs.alacritty = {
-    enable = true;
-    settings = {
-      window.opacity = 1.0;
-     
-      font  = {
-        builtin_box_drawing = true;
-        normal = {
-          style = lib.mkForce "Bold";
+{lib, config, ... }:{
+
+  options = {
+    alacrittyMod = lib.mkEnableOption "enables alacrittyMod";
+  };
+
+  config = lib.mkIf config.alacrittyMod {
+    programs.alacritty = {
+      enable = true;
+      settings = {
+        window.opacity = 1.0;
+       
+        font  = {
+          builtin_box_drawing = true;
+          normal = {
+            style = lib.mkForce "Bold";
+          };
         };
       };
     };
   };
 }
-      
+
