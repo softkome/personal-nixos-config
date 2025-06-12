c863bdccfea0480564d148f42796223f67d1be96
softkome <softkome+git@gmail.com>
2025-06-12 22:30:20 +0000

    Update lazygit.nix

--- home-manager/modules/lazygit.nix
+++ home-manager/modules/lazygit.nix
@@ -1,13 +1,20 @@
-{
-  programs.lazygit = {
-    enable = true;
-    settings = {
-      gui.showIcons = true;
-      gui.theme = {
-        lightTheme = false;
-        activeBorderColor = [ "green" "bold" ];
-        inactiveBorderColor = [ "grey" ];
-        selectedLineBgColor = [ "blue" ];
+{ lib, config, ...}: {
+
+  options = {
+    lazygitMod.enable = lib.mkEnableOption "enables lazygitMod";
+  };
+
+  config = lib.mkIf config.lazygitMod.enable {
+    programs.lazygit = {
+      enable = true;
+      settings = {
+        gui.showIcons = true;
+        gui.theme = {
+          lightTheme = false;
+          activeBorderColor = [ "green" "bold" ];
+          inactiveBorderColor = [ "grey" ];
+          selectedLineBgColor = [ "blue" ];
+        };
       };
     };
   };
