92571096a98c1f7c67e0eebd10f71c2b6ddd8b9c
softkome <softkome+git@gmail.com>
2025-06-14 13:39:25 +0000

    Update stylix.nix

--- home-manager/modules/stylix.nix
+++ home-manager/modules/stylix.nix
@@ -1,74 +1,81 @@
-{ pkgs, inputs, ... }: {
+{ pkgs, inputs, lib, config, ... }: {
 
-  home.packages = with pkgs; [
-    dejavu_fonts
-    jetbrains-mono
-    noto-fonts
-    noto-fonts-lgc-plus
-    texlivePackages.hebrew-fonts
-    noto-fonts-emoji
-    font-awesome
-    powerline-fonts
-    powerline-symbols
-    pkgs.nerd-fonts.symbols-only
-  ];
+  options = {
+    stylixMod = lib.mkEnableOption "enables stylixMod";
+  };
 
-  stylix = {
-    enable = true;
-    polarity = "dark";
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
-
-    targets = {
-      neovim.enable = false;
-      waybar.enable = false;
-      wofi.enable = false;
-      hyprland.enable = false;
-      hyprlock.enable = false;
-      mako.enable = false;
-    };
-
-    cursor = {
-      name = "Bibata-Modern-Ice";
-      size = 20;
-      package = pkgs.bibata-cursors;
-    };
-
-    fonts = {
-      emoji = {
-        name = "Noto Color Emoji";
-        package = pkgs.noto-fonts-color-emoji;
-      };
-      monospace = {
-        name = "JetBrains Mono";
-        package = pkgs.jetbrains-mono;
-      };
-      sansSerif = {
-        name = "Noto Sans";
-        package = pkgs.noto-fonts;
-      };
-      serif = {
-        name = "Noto Serif";
-        package = pkgs.noto-fonts;
-      };
-
-      sizes = {
-        terminal = 13;
-        applications = 11;
-      };
-    };
-
-    iconTheme = {
+  config = lib.mkIf config.stylixMod { 
+    home.packages = with pkgs; [
+      dejavu_fonts
+      jetbrains-mono
+      noto-fonts
+      noto-fonts-lgc-plus
+      texlivePackages.hebrew-fonts
+      noto-fonts-emoji
+      font-awesome
+      powerline-fonts
+      powerline-symbols
+      pkgs.nerd-fonts.symbols-only
+    ];
+  
+    stylix = {
       enable = true;
-      package = pkgs.papirus-icon-theme;
-      dark = "Papirus-Dark";
-      light = "Papirus-Light";
-    };
-
-    image = pkgs.fetchurl {
-      url = "https://i.pximg.net/img-original/img/2024/08/23/21/21/14/121764304_p0.jpg";  #HATSUNE MIKUUUUUUUU
-      sha256 = "0pd0xabh5kqq5m22kjps71bk5z64r4phygjhciawdwcrzyf6hrc4";
-#      url = "https://raw.githubusercontent.com/dharmx/walls/main/anime/a_cartoon_of_a_woman_with_her_hands_on_her_face.jpg"; #crow painting wallpaper
-#      sha256 = "1zda8j5qk0jgiw9q06diwngv1kl14gsskxf810vgr7dv5xzdvnba";
+      polarity = "dark";
+      base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
+  
+     
+      targets = {
+        neovim.enable = false;
+        waybar.enable = false;
+        wofi.enable = false;
+        hyprland.enable = false;
+        hyprlock.enable = false;
+        mako.enable = false;
+      };
+  
+      cursor = {
+        name = "Bibata-Modern-Ice";
+        size = 20;
+        package = pkgs.bibata-cursors;
+      };
+  
+      fonts = {
+        emoji = {
+          name = "Noto Color Emoji";
+          package = pkgs.noto-fonts-color-emoji;
+        };
+        monospace = {
+          name = "JetBrains Mono";
+          package = pkgs.jetbrains-mono;
+        };
+        sansSerif = {
+          name = "Noto Sans";
+          package = pkgs.noto-fonts;
+        };
+        serif = {
+          name = "Noto Serif";
+          package = pkgs.noto-fonts;
+        };
+  
+        sizes = {
+          terminal = 13;
+          applications = 11;
+        };
+      };
+  
+      iconTheme = {
+        enable = true;
+        package = pkgs.papirus-icon-theme;
+        dark = "Papirus-Dark";
+        light = "Papirus-Light";
+      };
+  
+      image = pkgs.fetchurl {
+        url = "https://i.pximg.net/img-original/img/2024/08/23/21/21/14/121764304_p0.jpg";  #HATSUNE MIKUUUUUUUU
+        sha256 = "0pd0xabh5kqq5m22kjps71bk5z64r4phygjhciawdwcrzyf6hrc4";
+  #      url = "https://raw.githubusercontent.com/dharmx/walls/main/anime/a_cartoon_of_a_woman_with_her_hands_on_her_face.jpg"; #crow painting wallpaper
+  #      sha256 = "1zda8j5qk0jgiw9q06diwngv1kl14gsskxf810vgr7dv5xzdvnba";
+      };
     };
   };
 }
