70956def884a255dee71bdf904d36ab5795f0a94
softkome <softkome+git@gmail.com>
2025-06-14 23:02:21 +0000

    Update neovim.nix

--- home-manager/modules/neovim.nix
+++ home-manager/modules/neovim.nix
@@ -8,9 +8,6 @@
     programs.neovim = {
       enable = true;
       extraPackages = with pkgs; [
-        lua-language-server
-        python311Packages.python-lsp-server
-        nixd
         vimPlugins.nvim-treesitter-parsers.hyprlang
       ];
     };
