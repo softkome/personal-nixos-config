91566036ed82de9a4c8cabecc2d88322f30407e5
softkome <softkome+git@gmail.com>
2025-06-14 18:45:39 +0000

    Update home-packages.nix

--- home-manager/home-packages.nix
+++ home-manager/home-packages.nix
@@ -1,67 +1,59 @@
 { pkgs, ... }: {
+
   nixpkgs.config.allowUnfree = true;
 
   home.packages = with pkgs; [
 
     #desktop apps
-    librewolf
-    imv
-    mpv
-    obs-studio
-    pavucontrol
-    vesktop
-    stremio
+    librewolf #browser
+    imv #image viewer
+    mpv #video player
+    obs-studio #recording software
+    pavucontrol #volume control
+    vesktop #discord
+    stremio #steaming software
+    steam #game launcher
+    gamescope #game optimization
+    gamemode #game optimization
+    xfce.thunar #gui file manager
 
     #cli utils
-    bc
-    bottom
-    brightnessctl
-    cliphist
-    ffmpeg
-    ffmpegthumbnailer
-    fzf
-    git-graph
-    grimblast
-    htop
-    hyprpicker
-    ntfs3g
-    mediainfo
-    microfetch
-    playerctl
-    ripgrep
-    showmethekey
-    silicon
-    udisks
-    ueberzugpp
-    unzip
-    unrar
-    w3m
-    wget
-    wl-clipboard
-    wtype
-    yt-dlp
-    zip
-    cava
-    cmatrix
-    tt
-
-    #coding stuff
-    openjdk23
-    nodejs
-    python311
-    nixd
+    bc #calculator
+    bottom #system monitor
+    brightnessctl #brightness control
+    cliphist #clipboard manager
+    ffmpeg #multimedia framework
+    ffmpegthumbnailer #video thumbnailer
+    fzf #fuzzy finder
+    grimblast #screenshot helper
+    htop #process viewer
+    hyprpicker #colour picker
+    ntfs3g #ntfs driver
+    mediainfo #media information
+    microfetch #fetch
+    playerctl #media controler
+    ripgrep #text search
+    udisks #disk management
+    unzip #extract .zip
+    unrar #extract .rar
+    wget #file downloader
+    wl-clipboard #wayland clipboard
+    wtype #wayland keyboard typer
+    zip #zip archiver
+    cava #audio visulliser
+    cmatrix #matrix effect
+    cbonsai #bonsai tree
 
     #wm stuff
-    libsForQt5.xwaylandvideobridge
-    libnotify
-    xdg-desktop-portal-gtk
-    xdg-desktop-portal-hyprland
+    libsForQt5.xwaylandvideobridge #video sharing bridge
+    libnotify #notification daemon
+    xdg-desktop-portal-gtk #gtk portal backend
+    xdg-desktop-portal-hyprland #hyprland portal backend
 
     #other
-    bemoji
-    nix-prefetch-scripts
-    protonup
-    mangohud
-    scientifica
+    bemoji #emoji picker
+    nix-prefetch-scripts #nix fetch scripts
+    protonup #proton installer
+    mangohud #performance overlay
   ];
 }
